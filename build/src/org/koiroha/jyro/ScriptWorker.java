8acfc9d538476c0bbfd50ef473840ff7e702d972
torao <koiroha@gmail.com>
2011-07-03 22:29:46 +0900

    modified

---
+++ build/src/org/koiroha/jyro/ScriptWorker.java
@@ -0,0 +1,51 @@
+/* **************************************************************************
+ * Copyright (C) 2008 BJoRFUAN. All Right Reserved
+ * **************************************************************************
+ * This module, contains source code, binary and documentation, is in the
+ * BSD License, and comes with NO WARRANTY.
+ *
+ *                                                 torao <torao@bjorfuan.com>
+ *                                                       http://www.moyo.biz/
+ * $Id:$
+*/
+package org.koiroha.jyro;
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ScriptWorker: Script Worker
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ *
+ * <p>
+ * @version $Revision:$
+ * @author torao
+ * @since 2011/07/03 Java SE 6
+ */
+public class ScriptWorker implements Worker {
+
+	// ======================================================================
+	// Constructor
+	// ======================================================================
+	/**
+	 */
+	public ScriptWorker() {
+		return;
+	}
+
+	// ======================================================================
+	//
+	// ======================================================================
+	/**
+	 *
+	 * <p>
+	 * @param args
+	 * @return
+	 */
+	@Override
+	public Object exec(Object... args) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
