322c4f930e0529c29118f0587f1c43a003153ba5
torao <koiroha@gmail.com>
2011-07-03 17:11:16 +0900

    modify

--- build/src/org/koiroha/jyro/JyroFactory.java
+++ build/src/org/koiroha/jyro/JyroFactory.java
@@ -10,6 +10,7 @@
 package org.koiroha.jyro;
 
 import java.io.*;
+import java.net.*;
 import java.util.*;
 
 import javax.xml.xpath.*;
@@ -48,6 +49,30 @@ public class JyroFactory {
 	public static final String XMLNS10 = "http://www.koiroha.org/xmlns/jyro/configuration_1.0";
 
 	// ======================================================================
+	// Default Library Directory
+	// ======================================================================
+	/**
+	 * Library directory name to load as default. ${jyro.home}/{@value}
+	 */
+	public static final String DIR_LIB = "lib";
+
+	// ======================================================================
+	// Temporary Directory
+	// ======================================================================
+	/**
+	 * Temporary directory to place some work files. ${jyro.home}/{@value}
+	 */
+	public static final String DIR_TMP = "tmp";
+
+	// ======================================================================
+	// Lock Filename
+	// ======================================================================
+	/**
+	 * Lock filename that will be placed in temporary directory.
+	 */
+	public static final String FILE_LOCK = ".lock";
+
+	// ======================================================================
 	// Root Node
 	// ======================================================================
 	/**
@@ -76,7 +101,7 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * The constructor is hidden in this class.
-	 * 
+	 *
 	 * @param root root element of xml configuration
 	 * @param prop default properties
 	 */
@@ -105,7 +130,7 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * Parse xml configuration specified in constructor.
-	 * 
+	 *
 	 * @return Jyro instance
 	 * @throws XPathException invalid xpath (bug?)
 	 */
@@ -134,7 +159,7 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * Parse xml configuration specified in constructor.
-	 * 
+	 *
 	 * @return Jyro instance
 	 * @throws XPathException invalid xpath (bug?)
 	 */
@@ -148,14 +173,27 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * Build worker adapter.
-	 * 
+	 *
 	 * @param elem worker element
+	 * @param parent default class loader of this worker
 	 * @return worker
 	 * @throws XPathException invalid xpath
 	 */
-	private WorkerAdapter buildWorker(Element elem) throws XPathException {
-		
-		// retrieve classpath and extdirs parameter
+	private Worker buildWorker(Element elem, ClassLoader parent) throws XPathException {
+
+		// retrieve node-scope class loader
+		String classpath = elem.getAttribute("classpath");
+		String extdirs = elem.getAttribute("extdirs");
+		ClassLoader loader = getLibextLoader(classpath, extdirs, parent);
+
+		// build worker
+		String type = elem.getAttribyte("type");
+		// set minimum thread-pool size
+		if(elem.hasAttribute("min")){
+
+		}
+		int min = toInt("min", f(elem.getAttribute("min")));
+		int max = toInt("max", f(elem.getAttribute("max")));
 		return;
 	}
 
@@ -164,7 +202,7 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * Format specified string value with variable.
-	 * 
+	 *
 	 * @param value string to format
 	 * @return formatted string
 	 */
@@ -173,37 +211,50 @@ public class JyroFactory {
 	}
 
 	// ======================================================================
-	// Create Instance
+	// Format String
 	// ======================================================================
 	/**
-	 * Create Jyro instance from specified stream.
-	 * 
-	 * @param in input stream of configuration xml
-	 * @return Jyro instance
-	 * @throws IOException if fail to read
+	 * Format specified string value with variable.
+	 *
+	 * @param value string to format
+	 * @return formatted string
 	 */
-	public static Jyro createInstance(InputStream in) throws JyroException {
-		
+	private int toInt(String name, String value) throws JyroException{
+		try {
+			return Integer.parseInt(value);
+		} catch(NumberFormatException ex){
+			throw new JyroException(name + " must be integer: " + value);
+		}
 	}
 
 	// ======================================================================
 	// Create Instance
 	// ======================================================================
 	/**
-	 * Create Jyro instance from specified DOM node.
-	 * 
-	 * @param node xml node of configuration
+	 * Create Jyro instance from specified jyro.home directory.
+	 *
+	 * @param dir jyro.home directory
+	 * @param parent default class loader
 	 * @return Jyro instance
-	 * @throws JyroException if fail to build instance
+	 * @throws JyroException if fail to configure
 	 */
-	public static Jyro createInstance(org.w3c.dom.Node node) throws JyroException {
-		try {
-			
-		} catch(XPathException ex){
-			logger.error("unexpected exception, this maybe bug; " + ex);
-			throw new IllegalStateException(ex);
+	public static Jyro createInstance(File dir, ClassLoader parent) throws JyroException {
+
+		// set default class loader if not specified
+		if(parent == null){
+			parent = Thread.currentThread().getContextClassLoader();
+			if(parent == null){
+				parent = ClassLoader.getSystemClassLoader();
+			}
 		}
-		return null;
+
+		// get class loader for extra libraries
+		String classpath = "";
+		String libext = new File(dir, DIR_LIB).toString();
+		ClassLoader loader = getLibextLoader(classpath, libext, parent);
+
+		// create Jyro instance
+		return new Jyro(dir, loader);
 	}
 
 	// ======================================================================
@@ -211,7 +262,7 @@ public class JyroFactory {
 	// ======================================================================
 	/**
 	 * Create Jyro instance from specified DOM element with properties.
-	 * 
+	 *
 	 * @param config xml element of configuration
 	 * @param prop default properties
 	 * @return Jyro instance
@@ -219,12 +270,80 @@ public class JyroFactory {
 	 */
 	public static Jyro createInstance(Element config, Properties prop) throws JyroException {
 		try {
-			
+
 		} catch(XPathException ex){
 			logger.error("unexpected exception, this maybe bug; " + ex);
 			throw new IllegalStateException(ex);
 		}
 		return null;
+	}
+
+	// ======================================================================
+	// Create Libext ClassLoader
+	// ======================================================================
+	/**
+	 * Create ClassLoader for specified libext directory.
+	 *
+	 * @param classpath classpath that separated pathseparator.
+	 * @param libext libext directories
+	 * @param parent parent class loader
+	 * @return class loader
+	 */
+	private static ClassLoader getLibextLoader(String classpath, String libext, ClassLoader parent) {
+		// TODO Curretly, supports local filesystem only not as URL
+		List<File> libs = new ArrayList<File>();
+
+		// retrieve library or directory from classpath
+		StringTokenizer tk = new StringTokenizer(classpath, File.pathSeparator);
+		while(tk.hasMoreTokens()){
+			File file = new File(tk.nextToken());
+			if(! file.exists()){
+				logger.warn("path not found: " + file.getAbsolutePath());
+			} else {
+				libs.add(file);
+				logger.debug("use library: " + file.getAbsolutePath());
+			}
+		}
+
+		// retrieve extra library files
+		tk = new StringTokenizer(libext, File.pathSeparator);
+		while(tk.hasMoreTokens()){
+
+			// retrieve library files in directory
+			File dir = new File(tk.nextToken());
+			File[] files = dir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					name = name.toLowerCase();
+					return name.endsWith(".jar") || name.endsWith(".zip");
+				}
+			});
+
+			// return default class loader if library not found
+			if(files == null || files.length == 0){
+				logger.debug("extra library not found: " + dir);
+				continue;
+			}
+
+			// add files
+			for(File f: files){
+				libs.add(f);
+				logger.debug("use library: " + f.getAbsolutePath());
+			}
+		}
+
+		// create url array to library files
+		List<URL> urls = new ArrayList<URL>();
+		for(int i=0; i<libs.size(); i++){
+			try {
+				urls.add(libs.get(i).toURI().toURL());
+			} catch(MalformedURLException ex){
+				logger.warn(ex.toString());
+			}
+		}
+
+		// create class loader
+		return new URLClassLoader(urls.toArray(new URL[urls.size()]), parent);
 	}
 
 }
