7c1bcf58e68cca34ad3fdffeaa8f714642af5d63
Torao Takami <koiroha@gmail.com>
2011-08-08 19:50:27 +0900

    experimental worker implementation

---
+++ build/src.worker/org/koiroha/jyro/workers/crawler/CrawlerBotImpl.java
@@ -0,0 +1,281 @@
+/* **************************************************************************
+ * Copyright (C) 2008 BJoRFUAN. All Right Reserved
+ * **************************************************************************
+ * This module, contains source code, binary and documentation, is in the
+ * BSD License, and comes with NO WARRANTY.
+ *
+ *                                                 torao <torao@bjorfuan.com>
+ *                                                       http://www.moyo.biz/
+ * $Id:$
+*/
+package org.koiroha.jyro.workers.crawler;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.*;
+
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
+import org.koiroha.jyro.*;
+import org.koiroha.xml.Xml;
+import org.koiroha.xml.parser.HTMLDocumentBuilderFactory;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// CrawlerBotImpl: Crawler Robot Implementation
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * The implementation class of crawler bot.
+ *
+ * @version $Revision:$
+ * @author torao
+ * @since 2011/08/06 Java SE 6
+ */
+public class CrawlerBotImpl implements CrawlerBot {
+
+	// ======================================================================
+	// Log Output
+	// ======================================================================
+	/**
+	 * Log output of this class.
+	 */
+	private static final Logger logger = Logger.getLogger(CrawlerBotImpl.class);
+
+	// ======================================================================
+	// Max Content Length
+	// ======================================================================
+	/**
+	 * Max content length as byte to read.
+	 */
+	private long maxContentLength = 1 * 1024 * 1024;
+
+	// ======================================================================
+	// Constructor
+	// ======================================================================
+	/**
+	 * Nothing to do in constructor.
+	 */
+	public CrawlerBotImpl() {
+		return;
+	}
+
+	// ======================================================================
+	// Constructor
+	// ======================================================================
+	/**
+	 * Nothing to do in constructor.
+	 */
+	public <T> T getInterface(Class<T> type){
+		return null;
+	}
+
+	// ======================================================================
+	// URL Content Retrieve Stage
+	// ======================================================================
+	/**
+	 * Retrieve content of specified object.
+	 *
+	 * @param content content to retrieve
+	 * @throws WorkerException if fail to retrieve
+	*/
+	public void retrieveContent(Content content) throws WorkerException{
+		InputStream in = null;
+		try {
+
+			// execute request
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(content.uri);
+			request.setHeader("User-Agent", "Mozilla/5.0 (compatible; Jyrobot/" + Jyro.VERSION + "; +http://www.koiroha.org/jyro.html)");
+			if(content.referer != null){
+				request.setHeader("Referer", content.referer.toASCIIString());
+			}
+
+			// read response content
+			HttpResponse response = client.execute(request);
+			HttpEntity entity = response.getEntity();
+			if(entity != null){
+				Header header = entity.getContentType();
+				if(header != null){
+					content.contentType = header.getValue();
+				}
+				in = entity.getContent();
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				byte[] buffer = new byte[1024];
+				long remaining = maxContentLength;
+				while(true){
+					int len = (int)Math.min(buffer.length, remaining);
+					len = in.read(buffer, 0, len);
+					if(len < 0){
+						break;
+					}
+					out.write(buffer, 0, len);
+					remaining -= len;
+				}
+				content.content = out.toByteArray();
+			}
+		} catch(Exception ex){
+			throw new WorkerException(ex);
+		}
+
+		getInterface(CrawlerBot.class).analyzeContent(content);
+		return;
+	}
+
+	// ======================================================================
+	// Content Analyzer Stage
+	// ======================================================================
+	/**
+	 * Parse content.
+	 *
+	 * @param content content to analyze
+	 * @throws WorkerException if fail to retrieve
+	*/
+	@Stage
+	public void analyzeContent(Content content) throws WorkerException {
+		List<URI> urls = new ArrayList<URI>();
+		try {
+			if(content.contentType.toLowerCase().startsWith("text/html")){
+
+				// parse html document
+				Charset def = Xml.getCharset(content.contentType);
+				HTMLDocumentBuilderFactory factory = new HTMLDocumentBuilderFactory();
+				InputSource is = factory.guessInputSource(new ByteArrayInputStream(content.content), def.name(), content.content.length);
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(is);
+
+				// extract link
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				NodeList nl = (NodeList)xpath.evaluate("//a/@href", doc, XPathConstants.NODESET);
+				for(int i=0; i<nl.getLength(); i++){
+					String href = ((Attr)nl.item(i)).getValue();
+					urls.add(content.uri.resolve(href));
+				}
+			}
+		} catch(Exception ex){
+			throw new WorkerException(ex);
+		}
+		return;
+	}
+
+	// ======================================================================
+	// Retrieve URLs
+	// ======================================================================
+	/**
+	 * Retrieve URLs that will used to specify uri as next content. You may
+	 * override this method and customize what this crawler focus for.
+	 *
+	 * Default behavior retrieve from all href attribute of &lt;a&gt; element.
+	 *
+	 * @param base base uri
+	 * @param doc document
+	 * @return collection of uri
+	*/
+	protected List<URI> extractLink(URI base, Document doc) {
+		return extractLink(base, doc, "//a/@href");
+	}
+
+	// ======================================================================
+	// Retrieve URLs
+	// ======================================================================
+	/**
+	 * Utility method to retrieve URLs that contained in specified document.
+	 *
+	 * @param base base uri
+	 * @param doc document
+	 * @param expr xpath expression of uri
+	 * @return collection of uri
+	*/
+	protected static List<URI> extractLink(URI base, Document doc, String... expr) {
+		List<URI> uris = new ArrayList<URI>();
+		try {
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			for(String e: expr){
+				NodeList nl = (NodeList)xpath.evaluate(e, doc, XPathConstants.NODESET);
+				for(int i=0; i<nl.getLength(); i++){
+					String href = nl.item(i).getTextContent();
+					uris.add(base.resolve(href));
+				}
+			}
+		} catch(XPathException ex){
+			throw new IllegalArgumentException(ex);
+		}
+		return uris;
+	}
+
+	// ======================================================================
+	// Determine Port
+	// ======================================================================
+	/**
+	 * Determine the port number of specified uri. Return negative value if
+	 * URI scheme is not supported.
+	 *
+	 * @param uri URI
+	 * @return port number
+	*/
+	private static int getPort(URI uri){
+		int port = uri.getPort();
+		if(port >= 0){
+			return port;
+		}
+		String scheme = uri.getScheme();
+		if(scheme.equals("http")){
+			return 80;
+		} else if(scheme.equals("https")){
+			return 443;
+		} else if(scheme.equals("ftp")){
+			return 21;
+		}
+		return -1;
+	}
+
+	// ======================================================================
+	// Receive specified job
+	// ======================================================================
+	/**
+	 *
+	 * @param job
+	 * @return
+	 * @throws WorkerException
+	*/
+	public List<URI> retrieveLink(String url) throws WorkerException {
+		logger.debug("running crawler to: " + url);
+
+		List<URI> urls = new ArrayList<URI>();
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(url);
+			request.setHeader("User-Agent", "Mozilla/5.0 (compatible; Jyrobot/" + Jyro.VERSION + "; +http://www.koiroha.org/jyro.html)");
+			HttpResponse response = client.execute(request);
+
+			Header header = response.getFirstHeader("Content-Type");
+			String contentType = header.getValue();
+			logger.debug("Content-Type: " + contentType);
+			if(contentType.toLowerCase().startsWith("text/html")){
+				DocumentBuilderFactory factory = new HTMLDocumentBuilderFactory();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(response.getEntity().getContent());
+
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				NodeList nl = (NodeList)xpath.evaluate("//a/@href", doc, XPathConstants.NODESET);
+				for(int i=0; i<nl.getLength(); i++){
+					String href = ((Attr)nl.item(i)).getValue();
+					urls.add(URI.create(url).resolve(href));
+				}
+			}
+		} catch(Exception ex){
+			throw new WorkerException(ex);
+		}
+		return urls;
+	}
+
+}
---
+++ build/src/org/koiroha/jyro/impl/JMSJobQueue.java
@@ -0,0 +1,179 @@
+/* **************************************************************************
+ * Copyright (C) 2008 BJoRFUAN. All Right Reserved
+ * **************************************************************************
+ * This module, contains source code, binary and documentation, is in the
+ * BSD License, and comes with NO WARRANTY.
+ *
+ *                                                 torao <torao@bjorfuan.com>
+ *                                                       http://www.moyo.biz/
+ * $Id:$
+*/
+package org.koiroha.jyro.impl;
+
+import javax.jms.*;
+
+import org.apache.log4j.Logger;
+import org.koiroha.jyro.*;
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// JMSJobQueue: JMS Job Queue
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * Job queue implementation by JMS.
+ *
+ * @version $Revision:$
+ * @author torao
+ * @since 2011/07/04 Java SE 6
+ */
+public class JMSJobQueue extends JobQueueImpl {
+
+	// ======================================================================
+	// Log Output
+	// ======================================================================
+	/**
+	 * Log output of this class.
+	 */
+	private static final Logger logger = Logger.getLogger(JMSJobQueue.class);
+
+	// ======================================================================
+	// Queue Session
+	// ======================================================================
+	/**
+	 * JMS Queue session of this queue.
+	 */
+	private final QueueSession session;
+
+	// ======================================================================
+	// Queue
+	// ======================================================================
+	/**
+	 * JMS Queue
+	 */
+	private final javax.jms.Queue queue;
+
+	// ======================================================================
+	// Queue Sender
+	// ======================================================================
+	/**
+	 * JMS queue sender.
+	 */
+	private final QueueSender sender;
+
+	// ======================================================================
+	// Queue Receiver
+	// ======================================================================
+	/**
+	 * JMS queue receiver.
+	 */
+	private final QueueReceiver receiver;
+
+	// ======================================================================
+	// Close Flag
+	// ======================================================================
+	/**
+	 *
+	 */
+	private boolean closed = false;
+
+	// ======================================================================
+	// Constructor
+	// ======================================================================
+	/**
+	 * @param id ID of this queue
+	 * @param con JMS Queue Connection
+	 * @throws JyroException fail to connect queue
+	 */
+	public JMSJobQueue(String id, QueueConnection con) throws JyroException {
+		super(id);
+		try {
+			this.session = con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			this.queue = session.createQueue(id);
+			this.sender = session.createSender(queue);
+			this.receiver = session.createReceiver(queue);
+		} catch(JMSException ex){
+			throw new JyroException(ex);
+		}
+		return;
+	}
+
+	// ======================================================================
+	// Post Job
+	// ======================================================================
+	/**
+	 * Post specified job to this queue.
+	 */
+	@Override
+	public void post(Job job) throws JyroException {
+		try {
+			Message msg = session.createObjectMessage(job);
+			sender.send(msg);
+		} catch(JMSException ex){
+			throw new JyroException(ex);
+		}
+		return;
+	}
+
+	// ======================================================================
+	// Constructor
+	// ======================================================================
+	/**
+	 */
+	@Override
+	public Job receive() throws JyroException {
+		try {
+			ObjectMessage msg = (ObjectMessage)receiver.receive();
+			return (Job)msg.getObject();
+		} catch(JMSException ex){
+			throw new JyroException(ex);
+		}
+	}
+
+
+	// ======================================================================
+	// Close Queue
+	// ======================================================================
+	/**
+	 * Close queue messaging and release resources.
+	 *
+	 * @throws JyroException if fail to close queue
+	*/
+	@Override
+	public void close() throws JyroException{
+
+		// set closed status first because normally exit should detect in receive()
+		synchronized(this){
+			if(this.closed){
+				return;
+			}
+			this.closed = true;
+		}
+
+		// close sender
+		try {
+			sender.close();
+		} catch(JMSException ex){
+			logger.error("fail to close JMS queue sender, try to continue", ex);
+		}
+
+		// close receiver
+		try {
+			receiver.close();
+		} catch(JMSException ex){
+			logger.error("fail to close JMS queue receiver, try to continue", ex);
+		}
+
+		// close session
+		try {
+			session.close();
+		} catch(JMSException ex){
+			throw new JyroException("fail to close JMS session", ex);
+		}
+
+		super.close();
+		return;
+	}
+
+}
